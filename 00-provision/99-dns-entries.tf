b41596cb403d590904ad3fcb49a072916aa1dcc5
Sylvain <s.metayer@groupeonepoint.com>
2024-08-04 10:24:49 +0200

    feat: add photoprism
    chore: update rclone config

--- 00-provision/99-dns-entries.tf
+++ 00-provision/99-dns-entries.tf
@@ -5,3 +5,15 @@ resource "ovh_domain_zone_record" "nextcloud" {
   ttl       = 300
   target    = hcloud_server.nextcloud.ipv4_address
 }
+
+data "http" "home_ip" {
+  url = "https://ipv4.icanhazip.com"
+}
+
+resource "ovh_domain_zone_record" "photos" {
+  zone      = "sylvain.cloud"
+  subdomain = "photos"
+  fieldtype = "A"
+  ttl       = 300
+  target    = chomp(data.http.home_ip.response_body)
+}
