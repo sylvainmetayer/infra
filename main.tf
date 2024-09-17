6ceeb8c2e4bfb577ff2ac763c361574c39cb6f49
Sylvain <s.metayer@groupeonepoint.com>
2024-09-17 14:25:55 +0200

    fix: wip old plan

--- main.tf
+++ main.tf
@@ -3,38 +3,38 @@ data "gandi_domain" "zone" {
 }
 
 # VPS on Hetzner
-module "gibbs" {
-  source    = "./modules/gibbs"
-}
+# module "gibbs" {
+#   source    = "./modules/gibbs"
+# }
+#
+# module "ansible_inventory" {
+#   source    = "./modules/ansible_inventory"
+#   server_ip = module.gibbs.server_ip
+# }
 
-module "ansible_inventory" {
-  source    = "./modules/ansible_inventory"
-  server_ip = module.gibbs.server_ip
-}
+# resource "gandi_livedns_record" "rss" {
+#   zone = data.gandi_domain.zone.id
+#   name = data.keepass_entry.rss_domain.url
+#   type = "A"
+#   ttl  = 300
+#   values = [
+#     module.gibbs.server_ip
+#   ]
+# }
 
-resource "gandi_livedns_record" "rss" {
-  zone = data.gandi_domain.zone.id
-  name = data.keepass_entry.rss_domain.url
-  type = "A"
-  ttl  = 300
-  values = [
-    module.gibbs.server_ip
-  ]
-}
+# data "healthchecksio_channel" "signal" {
+#   kind = "signal"
+# }
 
-data "healthchecksio_channel" "signal" {
-  kind = "signal"
-}
+# module "android" {
+#   source                 = "./modules/android"
+#   healthcheck_channel_id = data.healthchecksio_channel.signal.id
+# }
 
-module "android" {
-  source                 = "./modules/android"
-  healthcheck_channel_id = data.healthchecksio_channel.signal.id
-}
+# module "backup" {
+#   source = "./modules/backup"
+# }
 
-module "backup" {
-  source = "./modules/backup"
-}
-
-output "gibbs_ip" {
-  value = module.gibbs.server_ip
-}
+# output "gibbs_ip" {
+#   value = module.gibbs.server_ip
+# }
--- scripts/extract-secrets.sh
+++ scripts/extract-secrets.sh
@@ -10,6 +10,6 @@ if [[ "$#" -ne 1 ]]; then
     exit 1
 fi
 
-keepassxc-cli attachment-export "$1" '/infra/terraform_envrc' .envrc .envrc
-keepassxc-cli show -a password -s "$1" /infra/ansible/vault-string-password > vault.secret
+/usr/bin/flatpak run --branch=stable --arch=x86_64 --command=keepassxc-cli org.keepassxc.KeePassXC attachment-export "$1" '/infra/terraform_envrc' .envrc .envrc
+/usr/bin/flatpak run --branch=stable --arch=x86_64 --command=keepassxc-cli org.keepassxc.KeePassXC show -a password -s "$1" /infra/ansible/vault-string-password > vault.secret
 chmod 600 vault.secret
